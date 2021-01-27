53d75ecee8509f998c10b6991275e54d0164b4ec
paslavskyi9roman <paslavsky.roman@gmail.com>
2021-01-27 23:00:46 +0200

    v0.1.6: Moving Posts array from app to post.comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,12 @@
 import { Component } from '@angular/core';
 
-export interface Posts {
-  title: string,
-  text: string,
-  author: string
-}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  posts: Posts[] = [
-    {title: 'Post 1', text: 'lorem 1 text', author: 'Nickname'},
-    {title: 'Post 2', text: 'lorem 2 text', author: 'Nickname'},
-    {title: 'Post21', text: 'lorem 3 text', author: 'Nickname'},
-    {title: 'Post21', text: 'lorem 3 text', author: 'Nickname'},
-    {title: 'Post21', text: 'lorem 3 text', author: 'Nickname'},
-    {title: 'Post21', text: 'lorem 3 text', author: 'Nickname'},
-    {title: 'Post21', text: 'lorem 3 text', author: 'Nickname'},
-  ]
+  
 
 }
--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+export interface Posts {
+  title: string,
+  text: string,
+  author: string
+}
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
@@ -14,5 +18,16 @@ export class PostComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  posts: Posts[] = [
+    {title: 'Post 1', text: 'lorem 1 text', author: 'Nickname'},
+    {title: 'How to increase FPS in CyberPunk 2077', text: 'lorem 2 text', author: 'Nickname'},
+    {title: 'Post21', text: 'lorem 3 text', author: 'Nickname'},
+    {title: 'Post21', text: 'lorem 3 text', author: 'Nickname'},
+    {title: 'Post21', text: 'lorem 3 text', author: 'Nickname'},
+    {title: 'Post21', text: 'lorem 3 text', author: 'Nickname'},
+    {title: 'Post21', text: 'lorem 3 text', author: 'Nickname'},
+    {title: 'Post21', text: 'lorem 3 text', author: 'Nickname'},
+  ]
 
 }
