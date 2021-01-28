b9bc541ba7e811b2ce6dcccbf4f4d1880bd66adf
paslavskyi9roman <paslavsky.roman@gmail.com>
2021-01-28 20:17:47 +0200

    v0.1.8: Routing implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainScreenComponent } from './components/main-screen/main-screen.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { CreatePostComponent } from './components/create-post/create-post.component';
+import { ViewPostComponent } from './components/view-post/view-post.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path: '', component: MainScreenComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'create-post', component: CreatePostComponent},
+  {path: 'post', component: ViewPostComponent},
+  {path: '**', component: PageNotFoundComponent},
+
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { Routes, RouterModule } from '@angular/router'
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PostComponent } from './components/post/post.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainScreenComponent } from './components/main-screen/main-screen.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { CreatePostComponent } from './components/create-post/create-post.component';
+import { ViewPostComponent } from './components/view-post/view-post.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const appRoutes: Routes = [
-  {path: '', component: AppComponent},
-  {path: 'main', component: MainScreenComponent},
-]
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,6 +20,10 @@ const appRoutes: Routes = [
     PostComponent,
     FooterComponent,
     MainScreenComponent,
+    LoginComponent,
+    RegisterComponent,
+    CreatePostComponent,
+    ViewPostComponent,
   ],
   imports: [
     BrowserModule,
