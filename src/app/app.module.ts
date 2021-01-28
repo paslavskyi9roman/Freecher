7dcb98d5b6be850416df4a1e444b15afb525538d
paslavskyi9roman <paslavsky.roman@gmail.com>
2021-01-28 18:05:30 +0200

    v.0.1.7: Creating a mainScreen component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { Routes, RouterModule } from '@angular/router'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PostComponent } from './components/post/post.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MainScreenComponent } from './components/main-screen/main-screen.component';
 
+const appRoutes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'main', component: MainScreenComponent},
+]
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     PostComponent,
-    FooterComponent
+    FooterComponent,
+    MainScreenComponent,
   ],
   imports: [
     BrowserModule,
