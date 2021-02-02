7967c9b94950b3f9854596cb194055b4fb87ad64
paslavskyi9roman <paslavsky.roman@gmail.com>
2021-02-02 23:30:57 +0200

    v0.2.6: Adding Google SignIn authorization

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,9 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  userLogged = false
 
-  login() {
-    console.log('logged')
+  userLoggedHandler() {
+    this.userLogged = true
   }
 
   constructor() { }
