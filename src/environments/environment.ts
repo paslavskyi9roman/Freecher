3da48b176d30b5dce3589591f4565fa3f87acbb3
paslavskyi9roman <paslavsky.roman@gmail.com>
2021-02-04 23:26:58 +0200

    v0.2.9: Connecting Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+   firebase: {
+    apiKey: "AIzaSyAcqUouz0f_VctLVpLZYAIU-2vzjFGcHlI",
+    authDomain: "freecher-1.firebaseapp.com",
+    databaseURL: "https://freecher-1-default-rtdb.firebaseio.com",
+    projectId: "freecher-1",
+    storageBucket: "freecher-1.appspot.com",
+    messagingSenderId: "765155672773",
+    appId: "1:765155672773:web:a7dfc9a18f5ddebb3526ac",
+    measurementId: "G-FF4XM4G99Q"
+  }
 };
 
 /*
