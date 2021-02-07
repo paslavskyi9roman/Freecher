614f2b0b9e494eed045606831e1b268d4fc5f083
paslavskyi9roman <paslavsky.roman@gmail.com>
2021-02-07 16:26:16 +0200

    minor changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+   firebase: {
+    apiKey: "AIzaSyAcqUouz0f_VctLVpLZYAIU-2vzjFGcHlI",
+    authDomain: "freecher-1.firebaseapp.com",
+    databaseURL: "https://freecher-1-default-rtdb.firebaseio.com",
+    projectId: "freecher-1",
+    storageBucket: "freecher-1.appspot.com",
+    messagingSenderId: "765155672773",
+    appId: "1:765155672773:web:a7dfc9a18f5ddebb3526ac",
+    measurementId: "G-FF4XM4G99Q"
+  }
 };
