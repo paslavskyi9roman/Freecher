c9b04f7104f3cbbd582f2df14c2651c16eb38c21
paslavskyi9roman <paslavsky.roman@gmail.com>
2021-02-07 12:20:33 +0200

    v0.3.5: Adding functionality to Create-Post

--- src/app/components/create-post/create-post.component.ts
+++ src/app/components/create-post/create-post.component.ts
@@ -1,21 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PostService } from '../../services/post.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
   styleUrls: ['./create-post.component.scss']
 })
 export class CreatePostComponent implements OnInit {
+  public postForm: FormGroup;
 
-  addPost() {
-    console.log('add post clicked')
+  constructor(
+    public postService: PostService,
+    public formBuilder: FormBuilder,
+    public router: Router
+  ) { 
+    this.postForm = this.formBuilder.group({
+      title: [''],
+      text: [''],
+      author: ['']
+    })      
   }
-
-  constructor() 
-  { }
 
   ngOnInit(): void {
-    
   }
 
+  onSubmit() {
+    this.postService.createPost(this.postForm.value);
+    this.router.navigate(['']); 
+   };
 }
